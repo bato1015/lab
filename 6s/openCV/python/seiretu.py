5c9c028b0e6ee78b3c0f03f69dfb7cf691fb874e
Bato Riko <s20c1101jd@s.chib>
2023-02-19 17:58:58 +0900

    0219_boxelsuccess

--- 6s/openCV/python/seiretu.py
+++ 6s/openCV/python/seiretu.py
@@ -7,7 +7,7 @@ data = np.zeros((129,257,257), dtype=np.uint8)
 def depth_data(mat):
     with open('../shindokaku.txt', 'r', encoding='utf-8') as fin:  # ファイルを開く
         for line in fin.readlines():  # 行をすべて読み込んで1行ずつfor文で回す
-            
+
             row = []  # 行のデータを保存
             #row = np.array([300, 300, 300])
             toks = line.split(' ')  # 行をカンマで分割する
@@ -24,17 +24,21 @@ def depth_data(mat):
             mat.append(row)  # 行をnumsに保存
             
 
-depth_data(mat)
+depth_data(mat)# 関数
 arr_1d = np.array(mat)#list to array
-
-"""for height in range(arr_1d.shape[0]): 
-    for weight in range(arr_1d.shape[1]):
-        num_depth=round(arr_1d[height][weight]%(5/256))
-        data[height,weight,num_depth]=1"""
-np.savetxt('data2.txt',arr_1d,fmt='%f4')
 for height in range(arr_1d.shape[0]): 
     for weight in range(arr_1d.shape[1]):
-        arr_1d[height,weight]=round(arr_1d[height][weight]/(5/256)) 
-#print(arr_1d.shape[1])
-#print(data)
+        num_depth=round(arr_1d[height][weight]/(5/256))
+        data[height,weight,num_depth]=1
+
+np.savetxt('data2.txt',arr_1d,fmt='%f')
+for height in range(arr_1d.shape[0]): 
+    for weight in range(arr_1d.shape[1]):
+        w=round(arr_1d[height][weight]/(5/256))
+        arr_1d[height,weight]=w
+#print(data[61,125,226])
+print(data[59,123+9,199])
+#print(data.index(1))
+x=np.where(data==1)
+print(x)
 np.savetxt('data1.txt',arr_1d,fmt='%d')
